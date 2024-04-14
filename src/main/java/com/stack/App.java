556a5f26bbf3fa3120c10817273e87c5cb45b3b6
itschenxiang <itschenxiang@itschenxiangdeMacBook-Pro.local>
2024-04-14 22:30:52 +0800

    quick start of maven-jar-plugin

---
+++ src/main/java/com/stack/App.java
@@ -0,0 +1,15 @@
+package com.stack;
+
+import org.joda.time.LocalDateTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! " + new LocalDateTime());
+    }
+}
